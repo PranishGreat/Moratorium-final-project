25264a8472e74158cfd17ac3b46e5c79c022b6ef
Pranish <55677908+PranishGreat@users.noreply.github.com>
2021-05-12 17:15:15 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,651 @@
+// imports
+var mongoose = require("mongoose");
+const express= require('express');
+const app=  express();
+const bodyParser=require('body-parser')
+const session = require('express-session');
+const swal =require('sweetalert')
+var aes256 = require('aes256');
+const logger=require('./logger');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { createLogger } = require("winston");
+
+//Links
+const url = 'https://www.business-standard.com/topic/banking-sector'
+const url1 ='https://www.moneycontrol.com/personal-finance/banking/?classic=true'
+
+//Middle wares
+app.use(bodyParser.json()); 
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+
+app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+
+//Static Files
+app.use(express.static("public"));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/fonts', express.static(__dirname + 'public/fonts'));
+app.use('/assets', express.static(__dirname + 'public/assets'));
+app.use('/contactform', express.static(__dirname + 'public/contactform'));
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+// PORT 
+const port=8040;
+
+app.listen(port, ()=>logger.log('info',`Server connected to ${port}`));
+
+
+//Mongo Connection section
+mongoose.connect("mongodb+srv://abcd:abcd@cluster0.ksrqf.mongodb.net/myDB?retryWrites=true&w=majority", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+
+db.once("open", function() {
+  logger.log('info',"Connection Successful!");
+});
+//Mongo Connection section end
+
+//News
+var schema = mongoose.Schema({
+  text: { type: String},
+  Link1: { type: String},
+  time : { type : Date, default: Date.now }
+});
+// Mongooes cha model
+var Model = mongoose.model("model", schema, "news");
+
+
+//Mongo Schema 
+var userSchema = mongoose.Schema({
+  username:String,email:String,password:String,mobile:Number,name:String,dob:String,aadhar:Number,address:String,bank_name:String,moratorium_acc:Number,status:Boolean
+});
+
+
+//Mongo Model
+var userModel = mongoose.model('usermodel', userSchema,'user');
+
+var sess;
+var loginState=false;
+var registerState=false;
+var registerFail=false;
+var logout=false;
+var key = 'my passphrase';
+// ROUTES
+
+app.get("/",(req,res)=>{
+    var mysort = { time: -1 };
+    db.collection("news").find({}).sort(mysort).toArray(function(err, result) {
+      if (err) throw err;
+      loginState=false;
+      registerState=false;
+      registerFail=false;
+      logout=false;
+      res.render('index', { result:result ,status:result[0]['status'],state:loginState,regstate:registerState,regfail:registerFail,logout:logout});  
+    });
+});
+
+// index Route
+app.get("/index",(req,res)=>{
+    var mysort = { time: -1 };
+    db.collection("news").find({}).sort(mysort).toArray(function(err, result) {
+      if (err) throw err;
+      loginState=false;
+      registerState=false;
+      registerFail=false;
+      logout=false;
+      
+      res.render('index', { result:result,status:result[0]['status'],state:loginState,regstate:registerState,regfail:registerFail,logout:logout });
+    });
+});
+
+
+// login Route
+app.get('/login',(req,res)=>{
+  sess = req.session;
+    if(sess.email) {
+        console.log("User on Session:-  "+sess.email) 
+    }
+    else {
+        console.log('User Not Found');   
+        console.log('Please, Login first'); 
+        res.redirect('/index');
+    }
+  var mysort = { time: -1 };
+  loginState=false;
+  registerState=false;
+  registerFail=false;
+  logout=false;
+  var details=[];
+
+
+db.collection("moratorium").find({email:sess.email}).toArray(function(err, result1) {
+  if (err) throw err;
+  details=result1
+})
+
+   db.collection("user").find({email:sess.email}).sort(mysort).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(loginState)
+    result5=[{
+      username: result[0]['username'],
+      name: result[0]['name'],
+      address: result[0]['address'],
+      mobile: aes256.decrypt(key,result[0]['mobile']),
+      aadhar: aes256.decrypt(key,result[0]['aadhar']),
+      bank_name: aes256.decrypt(key,result[0]['bank_name']),
+      moratorium_acc: aes256.decrypt(key,result[0]['moratorium_acc'])    
+    }]
+    res.render('login', {result:result5,status:result[0]['status'],state:loginState,regstate:registerState,regfail:registerFail,logout:logout,details:details});
+
+})
+})
+
+app.get('/loginsuccess',(req,res)=>{
+
+    sess = req.session;
+      if(sess.email) {
+          logger.log('info',"Session Created for User:-  "+sess.email) 
+      }
+      else {
+          logger.log('error','User Not Found');   
+          logger.log('error','Please, Login first'); 
+          res.redirect('/index');
+      }
+      var details=[];
+    
+      db.collection("moratorium").find({email:sess.email}).toArray(function(err, result1) {
+        if (err) throw err;
+        details=result1
+      })
+
+    var mysort = { time: -1 };
+    loginState=true;
+     db.collection("user").find({email:sess.email}).sort(mysort).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(loginState)
+     
+      result5=[{
+        username: result[0]['username'],
+        name: result[0]['name'],
+        address: result[0]['address'],
+        mobile: aes256.decrypt(key,result[0]['mobile']),
+        aadhar: aes256.decrypt(key,result[0]['aadhar']),
+        bank_name: aes256.decrypt(key,result[0]['bank_name']),
+        moratorium_acc: aes256.decrypt(key,result[0]['moratorium_acc'])    
+      }]
+     
+      res.render('login', {result:result5,status:result[0]['status'],state:loginState,details:details});
+    });
+  })
+
+  app.get("/loginfail",(req,res)=>{
+    var mysort = { time: -1 };
+    db.collection("news").find({}).sort(mysort).toArray(function(err, result) {
+      if (err) throw err;
+      loginState=true;
+      registerState=false;
+      registerFail=false;
+      logout=false;
+      var details=[];
+      res.render('index', { result:result, status:result[0]['status'],state:loginState,regstate:registerState,regfail:registerFail,logout:logout,details:details});
+    });
+  });
+  
+  
+// home Route
+app.get('/home',(req,res)=>{
+  var mysort = { time: -1 };
+  db.collection("news").find({}).sort(mysort).toArray(function(err, result) {
+    if (err) throw err;
+    res.render('home', {result:result });
+  });
+});
+
+// register Route
+app.post('/register', urlencodedParser, function (req, res) {
+    var username = req.body.username; 
+    module.exports=username
+    var email =req.body.email1; 
+    var pass = req.body.password; 
+    var pass1 = req.body.password1; 
+    var status=false;
+     var encrypted = aes256.encrypt(key, pass);
+  var data =new userModel({ 
+      "username": username, 
+      "email": email, 
+      "password": encrypted,
+      "status":status
+  }) 
+ 
+if(pass===pass1){
+    db.collection('user').findOne({ email: email }, function(err, doc){
+      if(err) throw err;
+      if(!doc) {
+        console.log('Not Found:'+email)
+        db.collection('user').insertOne(data,function(err, collection){ 
+          if (err) throw err;
+          console.log(data);
+          logger.log('info',"Record inserted Successfully"); 
+          }); 
+        res.redirect('/registersuccess')
+      }else {
+          logger.log('info',"Found: " + email);
+          res.redirect('/registerfail')
+      }
+
+    });
+}else{
+  logger.log('error','Password not matched');
+  res.redirect('/registerfail')
+}
+})
+
+app.get("/registersuccess",(req,res)=>{
+  var mysort = { time: -1 };
+  db.collection("news").find({}).sort(mysort).toArray(function(err, result) {
+    if (err) throw err;
+    registerState=true;
+    loginState=false;
+    registerFail=false;
+    logout=false;
+    res.render('index', { result:result,regstate:registerState,state:loginState,regfail:registerFail,logout:logout });
+  });
+});
+
+app.get("/registerfail",(req,res)=>{
+  var mysort = { time: -1 };
+  db.collection("news").find({}).sort(mysort).toArray(function(err, result) {
+    if (err) throw err;
+    registerState=false;
+    loginState=false;
+    registerFail=true;
+    logout=false;
+    res.render('index', { result:result,regstate:registerState,state:loginState,regfail:registerFail,logout:logout });
+  });
+});
+
+// auth Route
+app.post('/auth', urlencodedParser, function (req, res) {
+  var email = req.body.login_email; 
+  var pass = req.body.login_pass; 
+  //aes256.decrypt(key, encrypted);
+  db.collection('user').findOne({  email: email}, function(err, doc){
+    if(err) throw err;
+    if(doc) {
+      if(aes256.decrypt(key, doc.password) === pass)
+      {
+        console.log("Login Successfully!!");
+        console.log("Welcome, "+email); 
+  
+        sess = req.session;
+        sess.email = req.body.login_email;
+        if(sess.email) {
+          return res.redirect('/loginsuccess');
+        }
+        res.redirect('/login');
+      }
+      else {
+        logger.log('error',"Wrong Credentials ");
+        res.redirect('/loginfail');
+      }
+      
+    } 
+    else {
+        logger.log('error',"Wrong Credentials ");
+        res.redirect('/loginfail')
+    }
+    
+});
+})
+
+// logout Route
+app.get('/logout',(req,res) => {
+    req.session.destroy((err) => {
+        if(err) {
+            return console.log(err);
+        }
+        logger.log('info','Logout!!')
+        logger.log('warn','Session Terminated!!!')
+        res.redirect('/logoutsuccess');
+    });
+
+});
+app.get("/logoutsuccess",(req,res)=>{
+  var mysort = { time: -1 };
+  db.collection("news").find({}).sort(mysort).toArray(function(err, result) {
+    if (err) throw err;
+    loginState=false;
+    registerState=false;
+    registerFail=false;
+    logout=true;
+    res.render('index', { result:result,state:loginState,regstate:registerState,regfail:registerFail,logout:logout });
+  });
+});
+
+// update Route
+app.post('/update', urlencodedParser, function (req, res) {
+  var name = req.body.update_name; 
+  var mobile = aes256.encrypt(key,req.body.update_mobile); 
+  var aadhar = aes256.encrypt(key,req.body.update_aadhar); 
+  var bank = aes256.encrypt(key,req.body.update_bank); 
+  var acc =aes256.encrypt(key,req.body.update_acc); 
+  var address = req.body.update_address; 
+  var dob = req.body.update_dob; 
+
+
+  var myquery = { email: sess.email };
+  var newvalues = { $set: {"name":name,"mobile":mobile,"dob":dob,"aadhar":aadhar,"address":address,"moratorium_acc":acc,"bank_name":bank,"status":true } };
+
+  db.collection("user").updateOne(myquery, newvalues, function(err,r) {
+    if (err) throw err;
+  
+    logger.log('info',"Information Updated!!");
+    
+  });
+  res.redirect('/login');  
+  });
+
+  // contact route
+  app.post('/contact', urlencodedParser, function (req, res) {
+    var contact_name = req.body.contact_name; 
+    var contact_email =req.body.contact_email; 
+    var contact_subject = req.body.contact_subject; 
+    var contact_message = req.body.contact_message; 
+    
+    // console.log("session in contact"+sess.email)
+     sess=req.session;
+     console.log(sess.email)
+  var data ={
+      "contact_name": contact_name, 
+      "contact_email": contact_email, 
+      "contact_subject": contact_subject,
+      "contact_message":contact_message }
+    db.collection('contact').insertOne(data,function(err,result){ 
+      if (err) throw err;
+      console.log(data);
+      logger.log('info',"Record inserted Successfully"); 
+      }); 
+      if(sess.email!=null){
+        res.redirect('/home')
+      }else{
+      res.redirect('/index')
+      }
+})
+
+//News Routes
+app.get('/url',(req,res,next)=>{
+  axios(url)
+  .then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html)
+    const l=[];
+    $('.listing-txt').each(function(){
+      var text=$(this).find('a').text();
+      var link ="https://www.business-standard.com/"+$(this).find('a').attr('href');
+
+      console.log(text,link);
+
+      if (text!=undefined || link!='' || text!=''){
+        var d=new Model({text:text,Link1:link});
+        d.save(function(err, doc) {
+          if (err) return console.error(err);
+          // console.log("Document inserted succussfully!");
+          console.log(doc)
+          
+          });    
+      }
+
+    })
+   
+}).catch(console.error);
+})
+
+app.get('/url1',(req,res,next)=>{
+ axios(url1)
+  .then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html)
+    const l=[];
+    $('.news_heading').each(function(){
+      var text=$(this).find('a').attr('title');
+      var link ='https://www.moneycontrol.com/'+ $(this).find('a').attr('href');
+      // var date1 =$(this).find('.storyDate').text();
+      if($(this).find('a').attr('href')!=undefined){
+        var d=new Model({text:text,Link1:link});
+        d.save(function(err, doc) {
+          if (err) return console.error(err);
+          console.log(doc);
+          });  
+      }
+        
+    })
+  
+}).catch(console.error);
+
+})
+
+
+//Admin Routes
+app.get('/admin_index', (req, res) =>{
+  // Count the total documents
+  var approved_count=null;
+  var pending_count=null;
+  var rejected_count=null;
+  var user_count=null;
+  var contact_count=null;
+  //Approved Count
+  db.collection('moratorium').countDocuments({status:"Approved"}, function (err, count) {
+      if (err){
+          console.log(err)
+      }else{
+          approved_count=count; 
+      }
+  });
+  //Rejected Count
+  db.collection('moratorium').countDocuments({status:"Rejected"}, function (err, count) {
+      if (err){
+          console.log(err)
+      }else{
+          rejected_count=count; 
+      }
+  });
+  //User Count
+  db.collection('admin').countDocuments({}, function (err, count) {
+      if (err){
+          console.log(err)
+      }else{
+          user_count=count; 
+      }
+  });
+  //Contact Count
+  db.collection('contact').countDocuments({}, function (err, count) {
+      if (err){
+          console.log(err)
+      }else{
+          contact_count=count; 
+      }
+  });
+  //Pending Count
+  db.collection('moratorium').countDocuments({status:"Pending"}, function (err, count) {
+      if (err){
+          console.log(err)
+      }else{
+          pending_count=count;   
+      }
+      res.render('admin_index',{pending_count:pending_count,approved_count:approved_count,rejected_count:rejected_count,user_count:user_count,contact_count:contact_count})
+  });
+});
+
+//Admin Login
+app.get('/admin_login', (req, res) =>{
+  res.render('admin_login');
+});
+
+//Admin Contact 
+app.get('/admin_contact', (req, res) =>{
+  db.collection('contact').find({}).toArray(function(err, doc){
+      if (err) {
+          console.log(err);
+      } else {
+          res.render('admin_contact_section', { contact: doc})
+      }
+  });
+});
+
+//Pending Records Table
+app.get('/pending', (req, res) =>{
+  db.collection('moratorium').find({status:"Pending"}).toArray(function(err, doc){
+      if (err) {
+          console.log(err);
+      } else {
+          res.render('pending_table', { details: doc,status:"Pending"})
+      }
+  });
+});
+
+//Approved Records Table
+app.get('/approved', (req, res) =>{
+  db.collection('moratorium').find({status:"Approved"}).toArray(function(err, doc){
+      if (err) {
+          console.log(err);
+      } else {
+          res.render('approved_table', { details: doc,status:"Approved"})
+      }
+  });
+});
+
+//Rejected Records Table
+app.get('/rejected', (req, res) =>{
+  db.collection('moratorium').find({status:"Rejected"}).toArray(function(err, doc){
+      if (err) {
+          console.log(err);
+      } else {
+          res.render('rejected_table', { details: doc,status:"Rejected"})
+      }
+  });
+});
+
+//Approve Record by updating month
+app.post('/approve', urlencodedParser, function (req, res) {
+  var month_no = req.body.month_no; 
+  var loan_no = req.body.loan_no;
+  console.log(month_no);
+  console.log(req.body.loan_no); //prints john
+  var myquery = { loan_no: loan_no };
+  var newvalues = { $set: {month: month_no, status: "Approved" } };
+  db.collection("moratorium").updateOne(myquery, newvalues, function(err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+  });
+  res.redirect('/approved');  
+});
+
+//Reject Record
+app.post('/reject', urlencodedParser, function (req, res) {
+  var loan_no = req.body.loan_no;
+  console.log(req.body.loan_no); //prints john
+  var myquery = { loan_no: loan_no };
+  var newvalues = { $set: {status: "Rejected" } };
+  db.collection("moratorium").updateOne(myquery, newvalues, function(err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+  });
+  res.redirect('/rejected');  
+});
+
+//Admin Login Authentication
+app.post('/admin_auth', urlencodedParser, function (req, res) {
+  var adminid = req.body.admin_id; 
+  var adminpass = req.body.admin_password; 
+  console.log(adminid);
+  console.log(adminpass);
+  db.collection('admin').findOne({  adminid: adminid, password: adminpass }, function(err, doc){
+    if(err) throw err;
+    if(doc) {
+      console.log("Login Successfully!!");
+      res.redirect('/admin_index')
+    } else {
+        console.log("Wrong Credentials ");
+        res.redirect('/admin_login')
+    }      
+});
+});
+
+
+//Andriod API's
+
+// login Route
+app.post('/api_auth',urlencodedParser,function (req, res) {
+  var email = req.body.email; 
+  var pass = req.body.password;
+  //Getting particular moratorium details of user
+  db.collection("moratorium").find({email:email}).toArray(function(err, result1) {
+    if (err) throw err;
+    details=result1
+  })
+  //Getting details of user
+  db.collection("user").find({email:email}).toArray(function(err, result) {
+    if (err) throw err;
+    userdata=result
+  })  
+  //Checking Credentials for User Login
+  db.collection('user').findOne({email: email, password: pass }, function(err, doc){
+    if(err) throw err;
+    if(doc) {
+
+      res.send({login_status:"Success",userdata:userdata,details:details})
+    } else {
+
+        console.log(email,pass)
+        res.send({login_status:"Fail"})
+    }
+});
+})
+
+app.post('/api_reg', urlencodedParser, function (req, res) {
+  var username = req.body.username; 
+  var email =req.body.email1; 
+  var pass = req.body.password; 
+  var status=false;
+var data =new userModel({ 
+    "username": username, 
+    "email": email, 
+    "password": pass,
+    "status":status
+}) 
+  db.collection('user').findOne({ email: email }, function(err, doc){
+    if(err) throw err;
+    if(!doc) {
+      db.collection('user').insertOne(data,function(err, collection){ 
+        if (err) throw err;
+        console.log(data);
+        logger.log('info',"Record inserted Successfully"); 
+        }); 
+      res.send({register_status:"Success",userdata:data})
+    }else {
+        logger.log('info',"Found: " + email);
+        res.send({register_status:"Fail,Email already exist!"})
+    }
+
+  });
+
+})
+
+app.get("/api_news",(req,res)=>{
+  var mysort = { time: -1 };
+  db.collection("news").find({}).sort(mysort).toArray(function(err, result) {
+    if (err) throw err;
+    loginState=false;
+    registerState=false;
+    registerFail=false;
+    logout=false;
+    res.send({news:result});  
+  });
+})
---
+++ public/assets/js/chatbot.js
@@ -0,0 +1,193 @@
+console.log("chat2.js");
+
+function extra() {
+  var u = document.getElementById("messages");
+  var listNode = document.createElement("li");
+  listNode.className = "message left appeared";
+
+  divAvatar = document.createElement("div");
+  divAvatar.className = "avatar";
+  divWrap = document.createElement("div");
+  divWrap.className = "text_wrapper";
+  divText = document.createElement("div");
+  divText.className = "text";
+
+  listNode.appendChild(divAvatar);
+  listNode.appendChild(divWrap);
+  divWrap.appendChild(divText);
+  console.log(u);
+  u.appendChild(listNode);
+}
+function setUserResponce(text) {
+  var ele =
+    '<li class="message right appeared"><div class="avatar"><img class="imgs" src="img/user.png" /></div><div class="text_wrapper"><div class="text">' +
+    text +
+    "</div></div></li>";
+	$(ele).appendTo(".messages");
+  cleartext();
+  scrollToBottomOfResults();
+  
+  callToApi(text);
+  
+}
+
+function setBotResponce(test) {
+  console.log(test);
+  var ele =
+    '<li class="message left appeared"><div class="avatar"><img class="imgs" src="img/bot.jpg" /></div><div class="text_wrapper"><div class="text">' +
+    test +
+    "</div></div></li>";
+	$(ele).appendTo(".messages");
+  scrollToBottomOfResults();
+}
+
+function setBotResponceBtn(val) {
+  console.log(val.title);
+  var ele =
+    '<li class="message left appeared">' +
+    '<div class="singleCard">' +
+    '<div class="suggestions">' +
+    '<div class="menu">' +
+    '<div class="menuChips" data-payload="' +
+    val.payload +
+    '">' +
+    val.title +
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    " </div>" +
+    "</li>";
+
+  $(ele).appendTo(".messages");
+  scrollToBottomOfResults();
+}
+
+function setBotResponceImg(val) {
+  var v =
+    `<li class="message left appeared">
+          <div class="singleCard">
+					<img class="imgcard" src="` +
+    val +
+    `"alt="` +
+    val +
+    `">
+						</div><div class="clearfix">
+        </li>`;
+  $(v).appendTo(".messages").prop("messages.scrollHeight");
+
+}
+
+
+// window.onload = function () {
+//   setBotResponce();
+// };
+
+async function callToApi(text) {
+  data = {
+    sender: "test_user",
+    message: text,
+  };
+  url = "https://eritodypimrnlu.herokuapp.com/webhooks/rest/webhook";
+  option = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  response = await fetch(url, option);
+  ResponseData = await response.json();
+  console.log(ResponseData);
+  filterBot(ResponseData);
+}
+// setBotResponce("Hello");
+// TODO: Add scroll to end
+// TODO: connect to api
+
+function filterBot(val) {
+  for (i = 0; i < val.length; i++) {
+    if (val[i].hasOwnProperty("text")) {
+      setBotResponce(val[i].text);
+    }
+    if (val[i].hasOwnProperty("image")) {
+      setBotResponceImg(val[i].image);
+    }
+    if (val[i].hasOwnProperty("buttons")) {
+      addSuggestion(val[i].buttons);
+    }
+  }
+}
+
+function addSuggestion(buttons) {
+  var suggestions = buttons;
+  var suggLength = buttons.length;
+  for (i = 0; i < suggLength; i++) {
+    setBotResponceBtn(suggestions[i]);
+    console.log(suggestions[i].title + "from ");
+  }
+}
+$(document).on("click", ".menu .menuChips", function () {
+  var text = this.innerText;
+  var payload = this.getAttribute("data-payload");
+  console.log("button payload: ", this.getAttribute("data-payload"));
+  setUserResponce(text);
+  send(payload);
+  $(".suggestions").remove(); //delete the suggestions
+});
+
+function playAudio(url) {
+  new Audio(url).play();
+}
+
+window.onload = function () {
+  setTimeout(function () {
+    document.getElementById("popup").style.display = "block";
+    playAudio("./res/service.wav");
+  }, 3000);
+};
+
+
+function openChat() {
+	document.getElementById("openChat").style.display = "block";
+	document.getElementById("popup").style.display = "none";
+	setBotResponce("Hello");
+	callToApi("menu");
+  }
+
+function closeChat() {
+	console.log("click");
+	document.getElementById("openChat").style.display = "none";
+	document.getElementById("popup").style.display = "block";
+	setBotResponce("/stop");
+  }
+
+function cleartext(){
+	document.getElementById('userMessage').value = '';
+	console.log("text1");
+}
+
+function scrollToBottomOfResults() {
+	const messages = document.getElementById('msgul');
+	messages.scrollTop = messages.scrollHeight;
+  }
+
+//   $(".message_input").on("keydown", function(e){
+//     // if (e.which == 13){
+//         var text = $(this).val();
+//         // if (text !== ""){
+//             setUserResponce(text);
+//             //callToApi(text);          
+//     //         $(this).val('');
+//     //     }
+//     // // }
+// });
+
+// $('.message_input').keypress(function (e) {
+// 	if (e.which == 13) {
+// 		var text = $(this).val();
+// 		setUserResponce(text);
+// 		console.log("hihelllo");
+// 	  return false;    //<---- Add this line
+// 	}
+//   });
